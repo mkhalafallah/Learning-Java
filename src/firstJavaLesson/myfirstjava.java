586322a3f4ecdf0fd17a17f964e29b35f7dd7335
unknown <kazzow35@gmail.com>
2020-01-29 20:41:47 -0500

    first commit

---
+++ src/firstJavaLesson/myfirstjava.java
@@ -0,0 +1,53 @@
+package firstJavaLesson;
+
+public class myfirstjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	// Concatenation
+		
+		String carPrice = "30,000";
+		
+		String dealer = "Honda";
+				
+		String type = "Automatic";
+		
+		System.out.println("My " + dealer + " car is " +  type + " & it costs " + carPrice);
+		
+		////////////////////
+		
+	// Single statment
+		
+		String myName;
+		
+		myName = "Khalafallah";
+		
+		//////////////////
+	
+	// Multiple
+		
+		String myNameIs = "Khalafallah";
+		
+		System.out.println(myNameIs);
+				
+	
+		
+		/////////////////
+		
+		Integer objectNumber = new Integer (10);
+		
+		Integer objectNumber2 = new Integer (10);
+		
+		boolean c = (objectNumber .equals(objectNumber2));
+		
+		System.out.println(c);
+		
+		boolean b = (objectNumber == objectNumber2);
+		
+		System.out.println(b);
+		
+		
+	}
+
+}
